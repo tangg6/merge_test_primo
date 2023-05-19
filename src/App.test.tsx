50eecea1b7750954746fdbd370905e279489aed9
Tanabut <tangg6@outlook.com>
2023-05-19 18:41:05 +0700

    Add Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { merge } from './utils/merge';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('merge', () => {
+  it('merges two sorted collections correctly', () => {
+    const collection1 = [1, 3, 5, 7];
+    const collection2 = [2, 4, 6, 8];
+    const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+    const result = merge(collection1, collection2);
+
+    expect(result).toEqual(expected);
+  });
 });
